Fix normalization.

sleppy/cartesian.py
@@ -188,7 +188,7 @@ class slepian_basis_function(object):
         self._set_normalization_coef()
 
     def _set_normalization_coef(self):
-        self.normalization_coef = np.sqrt(np.sum(self.spectral_coefs*self.spectral_coefs))/2.*np.pi/2.*np.pi
+        self.normalization_coef = np.sqrt(np.sum(self.spectral_coefs*self.spectral_coefs))
 
     def __call__(self, x, y):
         gen = generate_2D_basis_functions(self.nmax, self.extent[0], self.extent[1])

sleppy/test/test.py
@@ -11,7 +11,7 @@ def check_orthogonality(domain, basis, tol=3e-2, nx=500, ny=500):
     max_error = 0
     for i, (eigVal_i, eigFun_i) in enumerate(basis):
         for j, (eigVal_j, eigFun_j) in enumerate(basis):
-            inner_product = np.sum(eigFun_i(xgrid, ygrid)*eigFun_j(xgrid, ygrid))*dx*dy*2.*np.pi
+            inner_product = np.sum(eigFun_i(xgrid, ygrid)*eigFun_j(xgrid, ygrid))*dx*dy
             if i != j:
                 max_error = max(inner_product, max_error)
                